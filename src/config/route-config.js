95cc406bf1db85acdbde7d1b32e9337b0296bed5
katkinso <katyratkinson@yahoo.com>
2019-06-15 16:57:51 -0700

    Checkpoint-The-Post-Resource-Part2

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,8 +5,11 @@ module.exports = {
     init(app){
       const staticRoutes = require("../routes/static");
       const topicRoutes = require("../routes/topics");
+      const postRoutes = require("../routes/posts");
+
 
       app.use(staticRoutes);
+      app.use(postRoutes);
       app.use(topicRoutes);
     }
   }
