32dbb963c73a4abc493913a5347907cd754270ae
katkinso <katyratkinson@yahoo.com>
2019-06-30 10:29:21 -0700

    Checkpoint-9-Authentication: Complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -7,11 +7,13 @@ module.exports = {
       const topicRoutes = require("../routes/topics");
       const postRoutes = require("../routes/posts");
       const flairRoutes = require("../routes/flair");
+      const userRoutes = require("../routes/users");
 
       app.use(staticRoutes);
       app.use(postRoutes);
       app.use(topicRoutes);
       app.use(flairRoutes);
+      app.use(userRoutes);
 
     }
   }
